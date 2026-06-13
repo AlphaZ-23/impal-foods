deb31ddb775c58c7cce563898d1b54ad237051e4
Harshit Kori <149556991+AlphaZ-23@users.noreply.github.com>
2026-06-13 16:34:48 +0530

    Update api.js

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://impal-foods.onrender.com//api',
 });
 
 // Attach token to every request if available
